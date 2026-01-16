85bc5efc5ef60a5b379c9f3a040e5e5369059dae
Shaikh Mubashir Ahmed <shaikhmubashirahmed123@gmail.com>
2026-01-16 19:40:38 +0530

    Add FifthTopic and SixthTopic classes with basic array examples

---
+++ FifthTopic.java
@@ -0,0 +1,11 @@
+public class FifthTopic {
+    // Array 
+    public static void main(String args[]){
+        int [] arr = {1,2,3,4,5};
+        System.out.println(arr.length);
+        for(int i = 0; i < 5; i++){
+            System.out.println(arr[i]);
+        }
+    }
+    
+}
